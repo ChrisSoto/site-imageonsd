a8e0ec737547176304f51f0775a54223cce7f709
chris soto <christopherrsoto@gmail.com>
2024-06-06 09:27:02 -0700

    missing title in services

--- src/_data/services.js
+++ src/_data/services.js
@@ -11,6 +11,7 @@ const services = [];
 for (let i = 0; i < allServices.length; i++) {
   services.push({
     "name": allServices[i],
+    "title": "[[city]] " + allServices[i] + " Services",
     "description": "Quality " + allServices[i] + " near [[city]]. <b>Personalize</b> your apparel with no minimum order requirements and enjoy great discounts on bulk purchases. Visit our store today!",
     "hero": "[[city]] " + allServices[i] + " Service Shop",
     "heroSub": "Dial us or swing by our [[city]] Store for Custom T-Shirt Printing & Embroidery Services.",
